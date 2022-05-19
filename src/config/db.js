856a1d06c02abb1459d993e62d29fe53936170fd
Avinash Kumar <97193810+avinash70008@users.noreply.github.com>
2022-05-20 02:28:39 +0530

    hi

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,8 @@
 
  const connectdb=()=>{
  
-     return mongoose.connect("mongodb+srv://avinash:avinash1@cluster0.9vruf.mongodb.net/?retryWrites=true&w=majority");
-    
+     return mongoose.connect("mongodb+srv://avinash:avinash1@cluster0.9vruf.mongodb.net/food?retryWrites=true&w=majority");
+  
 }
  
  module.exports=connectdb;
